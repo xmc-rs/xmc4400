535a8cd0ba058ccf0302e19cd5d4b5820e03b1d3
lucasbrendel <1765755+lucasbrendel@users.noreply.github.com>
2024-05-03 11:44:42 +0000

    feat: updating from newer svd2rust

--- src/scu_hibernate/lpacth1.rs
+++ src/scu_hibernate/lpacth1.rs
@@ -1,85 +1,85 @@
 #[doc = "Register `LPACTH1` reader"]
-pub type R = crate::R<Lpacth1Spec>;
+pub type R = crate::R<LPACTH1_SPEC>;
 #[doc = "Register `LPACTH1` writer"]
-pub type W = crate::W<Lpacth1Spec>;
+pub type W = crate::W<LPACTH1_SPEC>;
 #[doc = "Field `AHIBIO0LO` reader - Analog HIB_IO_0 Lower Threshold Value"]
-pub type Ahibio0loR = crate::FieldReader;
+pub type AHIBIO0LO_R = crate::FieldReader;
 #[doc = "Field `AHIBIO0LO` writer - Analog HIB_IO_0 Lower Threshold Value"]
-pub type Ahibio0loW<'a, REG> = crate::FieldWriter<'a, REG, 6>;
+pub type AHIBIO0LO_W<'a, REG> = crate::FieldWriter<'a, REG, 6>;
 #[doc = "Field `AHIBIO0HI` reader - Analog HIB_IO_0 Upper Threshold Value"]
-pub type Ahibio0hiR = crate::FieldReader;
+pub type AHIBIO0HI_R = crate::FieldReader;
 #[doc = "Field `AHIBIO0HI` writer - Analog HIB_IO_0 Upper Threshold Value"]
-pub type Ahibio0hiW<'a, REG> = crate::FieldWriter<'a, REG, 6>;
+pub type AHIBIO0HI_W<'a, REG> = crate::FieldWriter<'a, REG, 6>;
 #[doc = "Field `AHIBIO1LO` reader - Analog HIB_IO_1 Lower Threshold Value"]
-pub type Ahibio1loR = crate::FieldReader;
+pub type AHIBIO1LO_R = crate::FieldReader;
 #[doc = "Field `AHIBIO1LO` writer - Analog HIB_IO_1 Lower Threshold Value"]
-pub type Ahibio1loW<'a, REG> = crate::FieldWriter<'a, REG, 6>;
+pub type AHIBIO1LO_W<'a, REG> = crate::FieldWriter<'a, REG, 6>;
 #[doc = "Field `AHIBIO1HI` reader - Analog HIB_IO_1 Upper Threshold Value"]
-pub type Ahibio1hiR = crate::FieldReader;
+pub type AHIBIO1HI_R = crate::FieldReader;
 #[doc = "Field `AHIBIO1HI` writer - Analog HIB_IO_1 Upper Threshold Value"]
-pub type Ahibio1hiW<'a, REG> = crate::FieldWriter<'a, REG, 6>;
+pub type AHIBIO1HI_W<'a, REG> = crate::FieldWriter<'a, REG, 6>;
 impl R {
     #[doc = "Bits 0:5 - Analog HIB_IO_0 Lower Threshold Value"]
     #[inline(always)]
-    pub fn ahibio0lo(&self) -> Ahibio0loR {
-        Ahibio0loR::new((self.bits & 0x3f) as u8)
+    pub fn ahibio0lo(&self) -> AHIBIO0LO_R {
+        AHIBIO0LO_R::new((self.bits & 0x3f) as u8)
     }
     #[doc = "Bits 8:13 - Analog HIB_IO_0 Upper Threshold Value"]
     #[inline(always)]
-    pub fn ahibio0hi(&self) -> Ahibio0hiR {
-        Ahibio0hiR::new(((self.bits >> 8) & 0x3f) as u8)
+    pub fn ahibio0hi(&self) -> AHIBIO0HI_R {
+        AHIBIO0HI_R::new(((self.bits >> 8) & 0x3f) as u8)
     }
     #[doc = "Bits 16:21 - Analog HIB_IO_1 Lower Threshold Value"]
     #[inline(always)]
-    pub fn ahibio1lo(&self) -> Ahibio1loR {
-        Ahibio1loR::new(((self.bits >> 16) & 0x3f) as u8)
+    pub fn ahibio1lo(&self) -> AHIBIO1LO_R {
+        AHIBIO1LO_R::new(((self.bits >> 16) & 0x3f) as u8)
     }
     #[doc = "Bits 24:29 - Analog HIB_IO_1 Upper Threshold Value"]
     #[inline(always)]
-    pub fn ahibio1hi(&self) -> Ahibio1hiR {
-        Ahibio1hiR::new(((self.bits >> 24) & 0x3f) as u8)
+    pub fn ahibio1hi(&self) -> AHIBIO1HI_R {
+        AHIBIO1HI_R::new(((self.bits >> 24) & 0x3f) as u8)
     }
 }
 impl W {
     #[doc = "Bits 0:5 - Analog HIB_IO_0 Lower Threshold Value"]
     #[inline(always)]
     #[must_use]
-    pub fn ahibio0lo(&mut self) -> Ahibio0loW<Lpacth1Spec> {
-        Ahibio0loW::new(self, 0)
+    pub fn ahibio0lo(&mut self) -> AHIBIO0LO_W<LPACTH1_SPEC> {
+        AHIBIO0LO_W::new(self, 0)
     }
     #[doc = "Bits 8:13 - Analog HIB_IO_0 Upper Threshold Value"]
     #[inline(always)]
     #[must_use]
-    pub fn ahibio0hi(&mut self) -> Ahibio0hiW<Lpacth1Spec> {
-        Ahibio0hiW::new(self, 8)
+    pub fn ahibio0hi(&mut self) -> AHIBIO0HI_W<LPACTH1_SPEC> {
+        AHIBIO0HI_W::new(self, 8)
     }
     #[doc = "Bits 16:21 - Analog HIB_IO_1 Lower Threshold Value"]
     #[inline(always)]
     #[must_use]
-    pub fn ahibio1lo(&mut self) -> Ahibio1loW<Lpacth1Spec> {
-        Ahibio1loW::new(self, 16)
+    pub fn ahibio1lo(&mut self) -> AHIBIO1LO_W<LPACTH1_SPEC> {
+        AHIBIO1LO_W::new(self, 16)
     }
     #[doc = "Bits 24:29 - Analog HIB_IO_1 Upper Threshold Value"]
     #[inline(always)]
     #[must_use]
-    pub fn ahibio1hi(&mut self) -> Ahibio1hiW<Lpacth1Spec> {
-        Ahibio1hiW::new(self, 24)
+    pub fn ahibio1hi(&mut self) -> AHIBIO1HI_W<LPACTH1_SPEC> {
+        AHIBIO1HI_W::new(self, 24)
     }
 }
 #[doc = "LPAC Threshold Register 1\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`lpacth1::R`](R).  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`lpacth1::W`](W). You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
-pub struct Lpacth1Spec;
-impl crate::RegisterSpec for Lpacth1Spec {
+pub struct LPACTH1_SPEC;
+impl crate::RegisterSpec for LPACTH1_SPEC {
     type Ux = u32;
 }
 #[doc = "`read()` method returns [`lpacth1::R`](R) reader structure"]
-impl crate::Readable for Lpacth1Spec {}
+impl crate::Readable for LPACTH1_SPEC {}
 #[doc = "`write(|w| ..)` method takes [`lpacth1::W`](W) writer structure"]
-impl crate::Writable for Lpacth1Spec {
+impl crate::Writable for LPACTH1_SPEC {
     type Safety = crate::Unsafe;
     const ZERO_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
     const ONE_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
 }
 #[doc = "`reset()` method sets LPACTH1 to value 0"]
-impl crate::Resettable for Lpacth1Spec {
+impl crate::Resettable for LPACTH1_SPEC {
     const RESET_VALUE: u32 = 0;
 }
